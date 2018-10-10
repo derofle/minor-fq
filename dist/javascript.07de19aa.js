e145c5728169110716c8962a62f859a42ff7f4c0
Rolf Oldenkotte <info@rolfoldenkotte.nl>
2018-10-10 18:25:40 +0200

    Cleaned it up a bit and made more structure in the story structure
    Still needs a lot of optimizatin and cleaning up though.

--- dist/javascript.07de19aa.js
+++ dist/javascript.07de19aa.js
@@ -111,11 +111,11 @@ window.onload = function () {
 
   var storyCounter = 1; // In the Story Array the pages will be saved and be navigated
 
-  var storyArray = []; // Blank player name to fill in later on, its global so it can be used anywhere
+  var stories = []; // Blank player name to fill in later on, its global so it can be used anywhere
 
   var playerName = ''; // Blank role array to fill in alter on, its global so it can be used anywhere
 
-  var roleArray = [];
+  var crew = [];
   var roleCount = 0;
   var randomRole = {};
   var randomName = ''; // This is the constructor of the Dialogue Objects
@@ -135,25 +135,29 @@ window.onload = function () {
 
   function addDialogue(name, bg, text, type, code) {
     var d = new Dialogue(name, bg, text, type, code);
-    storyArray.push(d);
+    stories.push(d);
   } // This function clears the array and then adds the objects into the array again, can be called during the playthrough, to for example input the playerName
 
 
   function refreshDialogue() {
-    storyArray = [];
+    stories = []; // Ordinary world
+
     addDialogue("Joksin Schura", "Appartment", "Hello?! Hello! Can you hear me? If you can read this message, please hit next in the lower right to let me know if I am not just talking to myself!");
     addDialogue("Joksin Schura", "Appartment", "Oh, hi there! Good! Someone found my hidden message! I\u2019m Joksin Schura, and I need your help! But first, how can I call you? Please input your name on the next screen, so I know what to call you!");
-    addDialogue("Joksin Schura", "Appartment", "<b>Input your name!</b>", "mission", "<input id=\"inputName\">\n        <button id=\"inputNameButton\" type=\"button\">Submit</button>");
+    addDialogue("Joksin Schura", "Appartment", "<b>Input your name!</b>", "mission", "<input id=\"inputName\">\n                    <button id=\"inputNameButton\" type=\"button\">Submit</button>\n                    ");
     addDialogue("Joksin Schura", "Appartment", "Is it true that your name is ".concat(playerName, "? Otherwise click on the No button to go back and input your name again!"), "confirm");
-    addDialogue("Joksin Schura", "Appartment", "You were chosen to get this message and save Earth from this unfortunate fate, ".concat(playerName, "! You are the hero the universe and Earth needs!"));
+    addDialogue("Joksin Schura", "Appartment", "You were chosen to get this message and save Earth from this unfortunate fate, ".concat(playerName, "! You are the hero the universe and Earth needs!")); // Call to Adventure
+
     addDialogue("Joksin Schura", "Appartment", "Listen closely, ".concat(playerName, ", cause this message is only for you. You are the only one who can do this. You are the only one who can save Earth!\u201D"));
     addDialogue("Joksin Schura", "Boom", "There are plans from outer space to blow up the Earth to construct a themepark, called Snarfland, on the place that Earth is now! Even though I like themeparks, I don't like blowing up planets for it!");
     addDialogue("Joksin Schura", "Boom", "The ones behind these plans are called The Combined Supremacy, they convinced the Galactic Senate that Earth is a wasteland and there is no life to be found on it!");
     addDialogue("Joksin Schura", "Appartment", "But that's absoluteley not true, cause otherwise I would not have been able to communicate with you!");
-    addDialogue("Joksin Schura", "Appartment", "You are the chosen one to save the Earth from this unfortunate faith!, ".concat(playerName, "!"));
+    addDialogue("Joksin Schura", "Appartment", "You are the chosen one to save the Earth from this unfortunate faith!, ".concat(playerName, "!")); // Refusal of the Call
+
     addDialogue("Joksin Schura", "Appartment", "Sadly my knowledge about Earth also tells me that Earth does not have spaceship capable of going in to deep space just yet.");
     addDialogue("Joksin Schura", "Appartment", "But wait, someone else is intercepting my signal, maybe he can help you, ".concat(playerName, "!"));
-    addDialogue("Joksin Schura", "Appartment", "Decrypt coded message!</br> Apply cesarean cypher to the signal and find out who else is intercepting my signal! The signal = PIFJSLIQ", "mission", "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/856px-Caesar3.svg.png\" id=\"cypherimg\"></img>\n             <input id=\"missionone\">\n            <button id=\"missiononebutton\" type=\"button\">Submit</button>\n             <p id=\"wrongInput\"></p>\n         ");
+    addDialogue("Joksin Schura", "Appartment", "Decrypt coded message!</br> Apply cesarean cypher to the signal and find out who else is intercepting my signal! The signal = PIFJSLIQ", "mission", "<img src=\"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/856px-Caesar3.svg.png\" id=\"cypherimg\"></img>\n                    <input id=\"missionone\">\n                    <button id=\"missiononebutton\" type=\"button\">Submit</button>\n                    <p id=\"wrongInput\"></p>\n                    "); // Meeting the mentor
+
     addDialogue("Slimvolt", "Lab", "Oh hello there, ".concat(playerName, "! I'm Professor Slimvolt! I also retrieved the message from the alien Joksin and I think that I can help you!"));
     addDialogue("Slimvolt", "Lab", "I have a machine right here that can help you, wait let me get it, hold on for a second!");
     addDialogue("none", "Lab", "Here I got it!");
@@ -161,17 +165,17 @@ window.onload = function () {
     addDialogue("Slimvolt", "Lab", "I just made it yesterday and I could use some guinea pigs, erhh, brave spacefarers to try it out, like you ".concat(playerName, "!"));
     addDialogue("Slimvolt", "Lab", "But right now you guys do not seem up to that task, you don\u2019t seem like space farers and you don\u2019t have the resources to go in to space! So there is some stuff to do before I give you guys my Turbo Space Engine!");
     addDialogue("Slimvolt", "Lab", "You cannot do this alone, ".concat(playerName, ", you need people to help you, for in space. So it\u2019s up to you guys to find people who are willing to help you on your journey! Good luck!"));
-    addDialogue("Slimvolt", "Lab", "Here will be the mission where they have to gather a crew and give them roles.", 'mission', "<label id=\"labelName\">Name: </label><input id=\"nameRoleInput\">\n        <label id=\"labelRole\">Role: </label><input id=\"roleRoleInput\">\n        <button id=\"roleInputButton\" type=\"button\">Add to crew</button>\n        <button id=\"roleInputDoneButton\" type=\"button\">Done making a crew</button>\n         <p id=\"added\"></p>\n         <p id=\"showcrew\"></p>\n         ");
+    addDialogue("Slimvolt", "Lab", "Here will be the mission where they have to gather a crew and give them roles.", 'mission', "<label id=\"labelName\">Name: </label><input id=\"nameRoleInput\">\n                    <label id=\"labelRole\">Role: </label><input id=\"roleRoleInput\">\n                    <button id=\"roleInputButton\" type=\"button\">Add to crew</button>\n                    <button id=\"roleInputDoneButton\" type=\"button\">Done making a crew</button>\n                    <p id=\"added\"></p>\n                    <p id=\"showcrew\"></p>\n                    ");
     addDialogue("Slimvolt", "Lab", "Are you sure you got your whole crew, ".concat(playerName, "?"), "confirm");
     addDialogue("Slimvolt", "Lab", "I see, so this is your crew! I really like ".concat(randomName, " as the ").concat(randomRole, "!"));
-    console.log(storyArray);
+    console.log(stories);
   } // Calling the startup refresh dialogue function
 
 
   refreshDialogue(); // Getting the start button and using it to load the first object in the array
 
   document.getElementById('startButton').addEventListener("click", function () {
-    showDialogue(storyArray[0]);
+    showDialogue(stories[0]);
   }); // The actual function to show the objects on the screen
 
   function showDialogue(obj) {
@@ -259,13 +263,13 @@ window.onload = function () {
     function nextPage() {
       console.log(storyCounter);
 
-      if (storyCounter >= storyArray.length) {} else {
+      if (storyCounter >= stories.length) {} else {
         storyCounter = storyCounter + 1;
-        showDialogue(storyArray[storyCounter - 1]);
+        showDialogue(stories[storyCounter - 1]);
       }
 
       if (storyCounter >= 22) {
-        randomCrew = roleArray[Math.floor(Math.random() * roleArray.length)];
+        randomCrew = crew[Math.floor(Math.random() * crew.length)];
         randomName = randomCrew.name;
         randomRole = randomCrew.role;
         refreshDialogue();
@@ -280,7 +284,7 @@ window.onload = function () {
 
       if (storyCounter <= 1) {} else {
         storyCounter = storyCounter - 1;
-        showDialogue(storyArray[storyCounter - 1]);
+        showDialogue(stories[storyCounter - 1]);
       }
     } // This is a specific if statement for the 3rd page, so that you can input your name
 
@@ -292,7 +296,7 @@ window.onload = function () {
         playerName = str;
         refreshDialogue();
         storyCounter = storyCounter + 1;
-        showDialogue(storyArray[storyCounter - 1]);
+        showDialogue(stories[storyCounter - 1]);
       });
     } // This is a specific if statement for the 13th page, so that you can input the code and it checks if its right or wrong
 
@@ -317,15 +321,15 @@ window.onload = function () {
       document.getElementById("roleInputButton").addEventListener("click", function () {
         var name = document.getElementById("nameRoleInput").value;
         var role = document.getElementById("roleRoleInput").value;
-        roleArray.push({
+        crew.push({
           name: name,
           role: role
         });
         text = "Crewmember added";
         document.getElementById("added").innerHTML = text;
-        array = roleArray;
-        document.getElementById("showcrew").innerHTML = "".concat(roleArray[roleCount].name, " ").concat(roleArray[roleCount].role, " ");
-        console.log(roleArray);
+        array = crew;
+        document.getElementById("showcrew").innerHTML = "".concat(crew[roleCount].name, " ").concat(crew[roleCount].role, " ");
+        console.log(crew);
         roleCount++;
         console.log(roleCount);
       });
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,11 +7,11 @@ window.onload = function() {
     // StoryCounter keeps track of the position within the story without grasping into the story array.
     let storyCounter = 1;
     // In the Story Array the pages will be saved and be navigated
-    let storyArray = [];
+    let stories = [];
     // Blank player name to fill in later on, its global so it can be used anywhere
     let playerName = '';
     // Blank role array to fill in alter on, its global so it can be used anywhere
-    let roleArray = [];
+    let crew = [];
 
     let roleCount = 0;
 
@@ -31,34 +31,44 @@ window.onload = function() {
     // This calls the constructor and after that puts them in the Story Array
     function addDialogue (name, bg, text, type, code) {
         var d = new Dialogue(name, bg, text, type, code);
-        storyArray.push(d);
+        stories.push(d);
     }
 
 
     
     // This function clears the array and then adds the objects into the array again, can be called during the playthrough, to for example input the playerName
     function refreshDialogue () {
-        storyArray = [];
+        stories = [];
+
+        // Ordinary world
         addDialogue(`Joksin Schura`, `Appartment`, `Hello?! Hello! Can you hear me? If you can read this message, please hit next in the lower right to let me know if I am not just talking to myself!`);
         addDialogue(`Joksin Schura`, `Appartment`, `Oh, hi there! Good! Someone found my hidden message! I’m Joksin Schura, and I need your help! But first, how can I call you? Please input your name on the next screen, so I know what to call you!`);
         addDialogue(`Joksin Schura`, `Appartment`, `<b>Input your name!</b>`, `mission`, 
-        `<input id="inputName">
-        <button id="inputNameButton" type="button">Submit</button>`);
+                    `<input id="inputName">
+                    <button id="inputNameButton" type="button">Submit</button>
+                    `);
+
         addDialogue(`Joksin Schura`, `Appartment`, `Is it true that your name is ${playerName}? Otherwise click on the No button to go back and input your name again!`, `confirm`);
         addDialogue(`Joksin Schura`, `Appartment`, `You were chosen to get this message and save Earth from this unfortunate fate, ${playerName}! You are the hero the universe and Earth needs!`);
+
+        // Call to Adventure
         addDialogue(`Joksin Schura`, `Appartment`, `Listen closely, ${playerName}, cause this message is only for you. You are the only one who can do this. You are the only one who can save Earth!”`);
         addDialogue(`Joksin Schura`, `Boom`, `There are plans from outer space to blow up the Earth to construct a themepark, called Snarfland, on the place that Earth is now! Even though I like themeparks, I don't like blowing up planets for it!`);
         addDialogue(`Joksin Schura`, `Boom`, `The ones behind these plans are called The Combined Supremacy, they convinced the Galactic Senate that Earth is a wasteland and there is no life to be found on it!`);
         addDialogue(`Joksin Schura`, `Appartment`, `But that's absoluteley not true, cause otherwise I would not have been able to communicate with you!`);
         addDialogue(`Joksin Schura`, `Appartment`, `You are the chosen one to save the Earth from this unfortunate faith!, ${playerName}!`);
+
+        // Refusal of the Call
         addDialogue(`Joksin Schura`, `Appartment`, `Sadly my knowledge about Earth also tells me that Earth does not have spaceship capable of going in to deep space just yet.`);
         addDialogue(`Joksin Schura`, `Appartment`, `But wait, someone else is intercepting my signal, maybe he can help you, ${playerName}!`);
         addDialogue(`Joksin Schura`, `Appartment`, `Decrypt coded message!</br> Apply cesarean cypher to the signal and find out who else is intercepting my signal! The signal = PIFJSLIQ`, `mission`, 
-             `<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/856px-Caesar3.svg.png" id="cypherimg"></img>
-             <input id="missionone">
-            <button id="missiononebutton" type="button">Submit</button>
-             <p id="wrongInput"></p>
-         `);
+                    `<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2b/Caesar3.svg/856px-Caesar3.svg.png" id="cypherimg"></img>
+                    <input id="missionone">
+                    <button id="missiononebutton" type="button">Submit</button>
+                    <p id="wrongInput"></p>
+                    `);
+
+        // Meeting the mentor
         addDialogue(`Slimvolt`, `Lab`, `Oh hello there, ${playerName}! I'm Professor Slimvolt! I also retrieved the message from the alien Joksin and I think that I can help you!`);
         addDialogue(`Slimvolt`, `Lab`, `I have a machine right here that can help you, wait let me get it, hold on for a second!`);
         addDialogue(`none`, `Lab`, `Here I got it!`);
@@ -67,16 +77,17 @@ window.onload = function() {
         addDialogue(`Slimvolt`, `Lab`, `But right now you guys do not seem up to that task, you don’t seem like space farers and you don’t have the resources to go in to space! So there is some stuff to do before I give you guys my Turbo Space Engine!`);
         addDialogue(`Slimvolt`, `Lab`, `You cannot do this alone, ${playerName}, you need people to help you, for in space. So it’s up to you guys to find people who are willing to help you on your journey! Good luck!`);
         addDialogue(`Slimvolt`, `Lab`, `Here will be the mission where they have to gather a crew and give them roles.`, 'mission', 
-        `<label id="labelName">Name: </label><input id="nameRoleInput">
-        <label id="labelRole">Role: </label><input id="roleRoleInput">
-        <button id="roleInputButton" type="button">Add to crew</button>
-        <button id="roleInputDoneButton" type="button">Done making a crew</button>
-         <p id="added"></p>
-         <p id="showcrew"></p>
-         `);
-         addDialogue(`Slimvolt`, `Lab`, `Are you sure you got your whole crew, ${playerName}?`, `confirm`);
-         addDialogue(`Slimvolt`, `Lab`, `I see, so this is your crew! I really like ${randomName} as the ${randomRole}!`);
-        console.log(storyArray);
+                    `<label id="labelName">Name: </label><input id="nameRoleInput">
+                    <label id="labelRole">Role: </label><input id="roleRoleInput">
+                    <button id="roleInputButton" type="button">Add to crew</button>
+                    <button id="roleInputDoneButton" type="button">Done making a crew</button>
+                    <p id="added"></p>
+                    <p id="showcrew"></p>
+                    `);
+
+        addDialogue(`Slimvolt`, `Lab`, `Are you sure you got your whole crew, ${playerName}?`, `confirm`);
+        addDialogue(`Slimvolt`, `Lab`, `I see, so this is your crew! I really like ${randomName} as the ${randomRole}!`);
+        console.log(stories);
     }
     
     // Calling the startup refresh dialogue function
@@ -84,7 +95,7 @@ window.onload = function() {
 
     // Getting the start button and using it to load the first object in the array
     document.getElementById('startButton').addEventListener(`click`, function (){
-        showDialogue(storyArray[0]);
+        showDialogue(stories[0]);
     });
 
     // The actual function to show the objects on the screen
@@ -200,14 +211,14 @@ window.onload = function() {
             // This function is being called to go to the next page
             function nextPage() {
                 console.log(storyCounter);
-                if (storyCounter >= storyArray.length) {
+                if (storyCounter >= stories.length) {
                 } else {
                     storyCounter = storyCounter + 1;
-                    showDialogue(storyArray[storyCounter - 1]);
+                    showDialogue(stories[storyCounter - 1]);
                 }
 
                 if (storyCounter >= 22) {
-                    randomCrew = roleArray[Math.floor(Math.random()*roleArray.length)];
+                    randomCrew = crew[Math.floor(Math.random()*crew.length)];
                     randomName = randomCrew.name;
                     randomRole = randomCrew.role;
                     refreshDialogue();
@@ -222,7 +233,7 @@ window.onload = function() {
                 if (storyCounter <= 1) {
                 } else {
                     storyCounter = storyCounter - 1;
-                    showDialogue(storyArray[storyCounter - 1]);
+                    showDialogue(stories[storyCounter - 1]);
                 }
             }
 
@@ -234,7 +245,7 @@ window.onload = function() {
                     playerName = str;
                     refreshDialogue();
                     storyCounter = storyCounter + 1;
-                    showDialogue(storyArray[storyCounter - 1]);
+                    showDialogue(stories[storyCounter - 1]);
                     
                 })
                 
@@ -260,15 +271,15 @@ window.onload = function() {
                 document.getElementById("roleInputButton").addEventListener(`click`, function () {
                     let name = document.getElementById("nameRoleInput").value;
                     let role = document.getElementById("roleRoleInput").value;
-                    roleArray.push( {
+                    crew.push( {
                         name: name,
                         role: role
                     })
                     text = "Crewmember added";
                     document.getElementById("added").innerHTML = text;
-                    array = roleArray;
-                    document.getElementById("showcrew").innerHTML = `${roleArray[roleCount].name} ${roleArray[roleCount].role} `;
-                    console.log(roleArray);
+                    array = crew;
+                    document.getElementById("showcrew").innerHTML = `${crew[roleCount].name} ${crew[roleCount].role} `;
+                    console.log(crew);
                     roleCount++;
                     console.log(roleCount);
             });
